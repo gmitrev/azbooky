adc3859faab8cfdba8001f872acb26a4057aa0e5
gmitrev <gvmitrev@gmail.com>
2015-03-06 15:41:49 +0200

    Books and authors linked

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,10 @@
 class Book < ActiveRecord::Base
+  has_many :authorships, dependent: :destroy
+  has_many :authors, through: :authorships
+
+  attr_accessor :author_id
+
+  before_save do
+    authors << Author.find(author_id) rescue true
+  end
 end
